e174f320915256b81884483943a6189d10dc30bc
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2024-09-27 11:11:14 +0200

    Clean dependencies in main-owner-service

--- main-owner-service/api/src/main/java/module-info.java
+++ main-owner-service/api/src/main/java/module-info.java
@@ -1,15 +1,12 @@
-open module java.gcp.pubsub.main.owner.service.api.main {
-    requires java.gcp.pubsub.main.owner.service.domain.main;
+module java.gcp.pubsub.main.owner.service.api.main {
     requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.databind;
-    requires org.apache.commons.lang3;
+    requires java.gcp.pubsub.main.owner.service.domain.main;
     requires spring.beans;
     requires spring.boot;
     requires spring.boot.autoconfigure;
+    requires spring.cloud.gcp.pubsub;
     requires spring.context;
-    requires spring.web;
     requires spring.integration.core;
     requires spring.messaging;
-    requires jdk.unsupported;
-    requires spring.cloud.gcp.pubsub;
+    requires spring.web;
 }

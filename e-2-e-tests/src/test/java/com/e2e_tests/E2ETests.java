94f3b2690dfbdb41c118099499a2a73d517a8d35
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2024-10-04 12:10:56 +0200

    Prepare pubsub emulator - part 1

--- e-2-e-tests/src/test/java/com/e2e_tests/E2ETests.java
+++ e-2-e-tests/src/test/java/com/e2e_tests/E2ETests.java
@@ -1,10 +1,18 @@
 package com.e2e_tests;
 
+import com.e2e_tests.pubsub_emulator.PubSubEmulator;
+import com.e2e_tests.pubsub_emulator.PubSubEmulatorInitializer;
+import com.google.cloud.spring.pubsub.PubSubAdmin;
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,21 +22,54 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-//@SpringBootTest
-@AutoConfigureMockMvc
-@ContextConfiguration(classes = E2ETestConfiguration.class)
+//@RunWith(SpringRunner.class)
+//@AutoConfigureMockMvc
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = E2ETests.App.class)
+@ContextConfiguration(classes = E2ETestConfiguration.class, initializers = PubSubEmulatorInitializer.class)
 public class E2ETests {
 
+ /*   @SpringBootApplication
+    public static class App {
+
+        public static void main(String... args) {
+            SpringApplication.run(App.class, args);
+        }
+
+        @Bean
+        PubSubTopicClient pubSubClient(PubSubTemplate template) {
+            return new PubSubTopicClient(template, "instance", "topic");
+        }
+
+    }*/
+
+  //  @Autowired
+ //  PubSubTopicClient pubSubTopicClient;
     @Autowired
-    private MockMvc mockMvc;
+    PubSubAdmin admin;
+    @Autowired
+    PubSubTemplate template;
+
+
+    //  @Autowired
+    //  private MockMvc mockMvc;
+    @Before
+    public void preparePubsubEmulator() {
+        PubSubEmulator.createTopicAndSubscription(
+                "labeledOwner",
+                "main-owner-service-to-labeledOwner-subscription"
+        );
+        PubSubEmulator.createTopicAndSubscription(
+                "initialOwner",
+                "label-owner-service-to-initialOwner-subscription"
+        );
+    }
 
     @Test
     public void simleE2Etest() throws Exception {
         String mockBody = """
-                    {"id":7,"name":"name1","address":"address2","phone":"phone3","email":"email4"}""";
-        mockMvc.perform(uploadInitialOwner().contentType(MediaType.APPLICATION_JSON).content(mockBody))
-                .andExpect(status().isOk());
+                {"id":7,"name":"name1","address":"address2","phone":"phone3","email":"email4"}""";
+        //    mockMvc.perform(uploadInitialOwner().contentType(MediaType.APPLICATION_JSON).content(mockBody))
+        //           .andExpect(status().isOk());
     }
 
     private static MockHttpServletRequestBuilder uploadInitialOwner() {

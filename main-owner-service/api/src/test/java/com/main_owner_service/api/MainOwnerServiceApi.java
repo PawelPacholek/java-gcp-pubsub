d71d72c7cf90f9a3e60730e673ac9541d254768a
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2024-11-15 11:04:20 +0100

    Add run sub-module to main-owner-service

---
+++ main-owner-service/api/src/test/java/com/main_owner_service/api/MainOwnerServiceApi.java
@@ -0,0 +1,11 @@
+package com.main_owner_service.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainOwnerServiceApi {
+  public static void main(String[] args) {
+    SpringApplication.run(MainOwnerServiceApi.class, args);
+  }
+}
--- main-owner-service/run/src/main/java/com/main_owner_service/run/MainOwnerServiceApplication.java
+++ main-owner-service/run/src/main/java/com/main_owner_service/run/MainOwnerServiceApplication.java
@@ -1,4 +1,4 @@
-package com.main_owner_service.api;
+package com.main_owner_service.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

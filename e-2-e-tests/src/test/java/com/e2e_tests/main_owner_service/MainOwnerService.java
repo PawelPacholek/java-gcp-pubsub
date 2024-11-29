8bce4c8a08b234ef5f71c55e7e288996a8f1560a
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2024-11-29 12:06:40 +0100

    wip

--- e-2-e-tests/src/test/java/com/e2e_tests/main_owner_service/MainOwnerService.java
+++ e-2-e-tests/src/test/java/com/e2e_tests/main_owner_service/MainOwnerService.java
@@ -18,23 +18,28 @@ public class MainOwnerService {
                new JibImage(imageName, MainOwnerService::buildJibContainer);
 
        GenericContainer<?> container = new GenericContainer<>(mainOwnerServiceImage)
-               .withCommand("sleep infinity")
-               .withEnv("GOOGLE_CLOUD_PROJECT", "test-project")
-               .withEnv("GOOGLE_APPLICATION_CREDENTIALS", "path_to_file")
-               .withCopyFileToContainer(null, "path_to_file")
+            //   .withCommand("sleep infinity")
+               .withEnv("GOOGLE_CLOUD_PROJECT", "test-project-bla")
+          //     .withEnv("GOOGLE_APPLICATION_CREDENTIALS", "path_to_file")
+          //     .withCopyFileToContainer(null, "path_to_file")
                //.withCommand("gcloud auth login")
                //.withCommand("gcloud config set project local-axle-425708-t0")
-               .withExposedPorts(8080);
+               .withExposedPorts(8080)
+               ;
        container.start();
        return container;
    }
 
    private static JibContainerBuilder buildJibContainer(JibContainerBuilder builder) {
      return builder
-             .setEntrypoint()
+             .setEntrypoint(
+                     "java", "com/main_owner_service/run/MainOwnerServiceApplication"
+                  //   "java", "-jar", "run/build/libs/run-1.0.0-SNAPSHOT.jar"
+             )
              //.addEnvironmentVariable()
              //.setProgramArguments()
-             .setExposedPorts(Port.tcp(8080));
+             .setExposedPorts(Port.tcp(8080))
+             ;
    }
 
 }

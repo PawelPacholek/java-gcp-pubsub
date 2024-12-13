04be0c020459fbe7302122311de197d55832b4fe
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2024-12-13 11:54:54 +0100

    e_to_e tests: Add mock gcloud credentials

--- e-2-e-tests/src/test/java/com/e2e_tests/main_owner_service/MainOwnerService.java
+++ e-2-e-tests/src/test/java/com/e2e_tests/main_owner_service/MainOwnerService.java
@@ -1,14 +1,10 @@
 package com.e2e_tests.main_owner_service;
 
 import com.google.cloud.tools.jib.api.JibContainerBuilder;
-import com.google.cloud.tools.jib.api.buildplan.Port;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.jib.JibImage;
 import org.testcontainers.utility.MountableFile;
 
-import java.io.File;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class MainOwnerService {
@@ -16,18 +12,21 @@ public class MainOwnerService {
     public static GenericContainer<?> startContainer() {
         String imageName = "gcr.io/local-axle-425708-t0/main-owner-service";
 
-        //ImageFromDockerfile mainOwnerServiceImage = new ImageFromDockerfile(imageName);
         JibImage mainOwnerServiceImage =
                 new JibImage(imageName, MainOwnerService::buildJibContainer);
 
         MountableFile entrypoint =
                 MountableFile.forHostPath(Path.of("src/test/java/com/e2e_tests/main_owner_service/entrypoint.sh"));
 
+        MountableFile credentials =
+                MountableFile.forHostPath(Path.of("src/test/java/com/e2e_tests/main_owner_service/gcloud_mock_credentials.json"));
+        String credentialsPath = "/app/gcloud_mock_credentials.json";
+
         GenericContainer<?> container = new GenericContainer<>(mainOwnerServiceImage)
-                //.withCommand("sleep infinity")
                 .withEnv("GOOGLE_CLOUD_PROJECT", "test-project-bla")
-                //.withEnv("GOOGLE_APPLICATION_CREDENTIALS", "path_to_file")
+                .withEnv("GOOGLE_APPLICATION_CREDENTIALS", credentialsPath)
                 .withCopyFileToContainer(entrypoint, "/app/entrypoint.sh")
+                .withCopyFileToContainer(credentials, credentialsPath)
             //    .withExposedPorts(8080)
                 ;
         container.start();
@@ -35,18 +34,7 @@ public class MainOwnerService {
     }
 
     private static JibContainerBuilder buildJibContainer(JibContainerBuilder builder) {
-        return builder
-             //     .setEntrypoint("sudo chmod 777 /app/entrypoint.sh ; /app/entrypoint.sh")
-                .setEntrypoint("/app/entrypoint.sh")
-            //    .setEntrypoint(
-                 //       "java",
-                  //      "-cp",
-                 //       "@/app/jib-classpath-file",
-                 //       "com.main_owner_service.run.MainOwnerServiceApplication",
-                 //       ";",
-                //        "sleep",
-                //        "infinity"
-             //   )
+        return builder.setEntrypoint("/app/entrypoint.sh")
                 //.addEnvironmentVariable()
                 //.setProgramArguments()
               //  .setExposedPorts(Port.tcp(8080))

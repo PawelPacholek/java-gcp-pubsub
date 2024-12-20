f559f2cea0eee54b5c3f4c8bd4cb8bc8219cadff
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2024-12-20 10:38:44 +0100

    Switch to better http client

--- e-2-e-tests/src/test/java/com/e2e_tests/E2ETests.java
+++ e-2-e-tests/src/test/java/com/e2e_tests/E2ETests.java
@@ -5,10 +5,14 @@ import com.e2e_tests.pubsub_emulator.PubSubEmulator;
 import com.e2e_tests.pubsub_emulator.PubSubEmulatorInitializer;
 import com.google.cloud.spring.pubsub.PubSubAdmin;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.impl.io.DefaultClassicHttpRequestFactory;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+//import org.apache.http.impl.client.HttpClients;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.http.message.BasicHttpEntityEnclosingRequest;
 import org.apache.http.message.BasicHttpRequest;
 import org.junit.Before;
@@ -27,6 +31,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.testcontainers.containers.GenericContainer;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
 
 import static com.google.api.client.http.HttpMethods.POST;
@@ -40,66 +46,67 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(/*classes = E2ETestConfiguration.class, */initializers = PubSubEmulatorInitializer.class)
 public class E2ETests {
 
-    @SpringBootApplication
-    public static class App {
+  @SpringBootApplication
+  public static class App {
 
-        public static void main(String... args) {
-            SpringApplication.run(App.class, args);
-        }
-
-   //     @Bean
-     //   PubSubTopicClient pubSubClient(PubSubTemplate template) {
-    //        return new PubSubTopicClient(template, "instance", "topic");
-     //   }
-
+    public static void main(String... args) {
+      SpringApplication.run(App.class, args);
     }
+
+    //     @Bean
+    //   PubSubTopicClient pubSubClient(PubSubTemplate template) {
+    //        return new PubSubTopicClient(template, "instance", "topic");
+    //   }
+
+  }
 
   //  @Autowired
- //  PubSubTopicClient pubSubTopicClient;
-    @Autowired
-    PubSubAdmin admin;
-    @Autowired
-    PubSubTemplate template;
+  //  PubSubTopicClient pubSubTopicClient;
+  @Autowired
+  PubSubAdmin admin;
+  @Autowired
+  PubSubTemplate template;
 
 
-    //  @Autowired
-    //  private MockMvc mockMvc;
-    @Before
-    public void preparePubsubEmulator() {
-        PubSubEmulator.createTopicAndSubscription(
-                "labeledOwner",
-                "main-owner-service-to-labeledOwner-subscription"
-        );
-        PubSubEmulator.createTopicAndSubscription(
-                "initialOwner",
-                "label-owner-service-to-initialOwner-subscription"
-        );
+  //  @Autowired
+  //  private MockMvc mockMvc;
+  @Before
+  public void preparePubsubEmulator() {
+    PubSubEmulator.createTopicAndSubscription(
+      "labeledOwner",
+      "main-owner-service-to-labeledOwner-subscription"
+    );
+    PubSubEmulator.createTopicAndSubscription(
+      "initialOwner",
+      "label-owner-service-to-initialOwner-subscription"
+    );
+  }
+
+  @Test
+  public void simpleEndToEndTest() {
+
+    GenericContainer mainOwnerService = MainOwnerService.startContainer();
+
+    try {
+
+      int mappedPort = mainOwnerService.getMappedPort(8080);
+
+      String body = """
+        {"id":7,"name":"name1","address":"address2","phone":"phone3","email":"email4"}""";
+      String uri = "http://localhost:%s/upload-initial-owner".formatted(mappedPort);
+      ClassicHttpRequest apacheRequest = DefaultClassicHttpRequestFactory.INSTANCE.newHttpRequest("post", uri);
+      apacheRequest.setEntity(new StringEntity(body));
+
+      try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+        var response = httpClient.execute(apacheRequest, r -> r);
+        System.out.println(response);
+      } catch (IOException e) {
+        throw new UncheckedIOException(e.getMessage(), e);
+      }
+
+    } finally {
+      mainOwnerService.stop();
     }
-
-    @Test
-    public void simpleEndToEndTest() throws Exception {
-        var httpClient = HttpClients.createDefault();
-
-        GenericContainer mainOwnerService = MainOwnerService.startContainer();
-
-        try {
-
-            int mappedPort = mainOwnerService.getMappedPort(8080);
-
-            String body = """
-                    {"id":7,"name":"name1","address":"address2","phone":"phone3","email":"email4"}""";
-
-            var uploadInitialOwnerRequest = new BasicHttpEntityEnclosingRequest(
-                    POST,
-                    "http://localhost:%s/upload-initial-owner".formatted(mappedPort)
-            );
-            uploadInitialOwnerRequest.setEntity(new StringEntity(body, UTF_8));
-
-            var r = httpClient.execute(new HttpHost("localhost", mappedPort), uploadInitialOwnerRequest);
-            System.out.println(r);
-        } finally {
-            mainOwnerService.stop();
-        }
-    }
+  }
 
 }

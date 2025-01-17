db372fa835334683a7ce04c110a3d669f9f071e4
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-01-17 10:18:32 +0100

    Simplify package scanning

--- main-owner-service/run/src/main/java/com/main_owner_service/run/MainOwnerServiceApplication.java
+++ main-owner-service/run/src/main/java/com/main_owner_service/run/MainOwnerServiceApplication.java
@@ -3,7 +3,7 @@ package com.main_owner_service.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.main_owner_service.api", "com.main_owner_service.persistence"})
+@SpringBootApplication(scanBasePackages = {"com.main_owner_service"})
 public class MainOwnerServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(MainOwnerServiceApplication.class, args);

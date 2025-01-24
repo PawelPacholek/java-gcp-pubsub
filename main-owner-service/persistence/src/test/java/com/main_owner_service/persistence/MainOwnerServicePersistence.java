b64c40fdab6d9b6f2ef75fc0033456c5e3c4360e
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-01-24 11:41:58 +0100

    Add tests to main-owner-service:persistence

---
+++ main-owner-service/persistence/src/test/java/com/main_owner_service/persistence/MainOwnerServicePersistence.java
@@ -0,0 +1,11 @@
+package com.main_owner_service.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainOwnerServicePersistence {
+  public static void main(String[] args) {
+    SpringApplication.run(MainOwnerServicePersistence.class, args);
+  }
+}

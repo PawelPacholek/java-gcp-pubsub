2512d498b5036c748fe5ab7ffa87bd2814c9ef52
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-01-31 10:06:06 +0100

    Add @IntegrationComponentScan to main-owner-service to scan @MessagingGateway

--- main-owner-service/run/src/main/java/com/main_owner_service/run/MainOwnerServiceApplication.java
+++ main-owner-service/run/src/main/java/com/main_owner_service/run/MainOwnerServiceApplication.java
@@ -3,9 +3,11 @@ package com.main_owner_service.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
 
-@SpringBootApplication(scanBasePackages = {"com.main_owner_service"})
+@IntegrationComponentScan("com.main_owner_service")
+@SpringBootApplication(scanBasePackages = "com.main_owner_service")
 public class MainOwnerServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(MainOwnerServiceApplication.class, args);

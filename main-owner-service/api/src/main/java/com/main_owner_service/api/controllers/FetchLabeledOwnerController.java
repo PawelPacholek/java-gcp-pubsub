868e59a8fb88c5c03be0d859c050e183752fbf54
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-02-21 11:46:19 +0100

    Handle lack of labeled owner in FetchLabeledOwnerController

--- main-owner-service/api/src/main/java/com/main_owner_service/api/controllers/FetchLabeledOwnerController.java
+++ main-owner-service/api/src/main/java/com/main_owner_service/api/controllers/FetchLabeledOwnerController.java
@@ -5,9 +5,13 @@ import com.main_owner_service.domain.usecases.FetchLabeledOwnerUseCase;
 import com.main_owner_service.domain.models.LabeledOwner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestController
 public class FetchLabeledOwnerController {
@@ -21,6 +25,14 @@ public class FetchLabeledOwnerController {
   @GetMapping(value = "/fetch-labeled-owner/{id}")
   public ResponseEntity<String> fetchOwner(@PathVariable("id") Long ownerId) {
     LabeledOwner labeledOwner = fetchOwnerUseCase.fetchLabeledOwner(ownerId);
+    if (labeledOwner == null)
+      throw new IllegalArgumentException("LabeledOwner not found");
     return new ResponseEntity<>(DataClassSerialization.serialize(labeledOwner), HttpStatus.OK);
   }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  static ResponseEntity<String> handleMalformedBody(IllegalArgumentException exception) {
+    return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
 }

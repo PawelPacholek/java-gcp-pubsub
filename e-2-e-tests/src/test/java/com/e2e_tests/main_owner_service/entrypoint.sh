e9ce30231226ff0fb17a4b1407c28b23c064d483
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-03-21 11:30:03 +0100

    Main owner service - allow for remote debugging

--- e-2-e-tests/src/test/java/com/e2e_tests/main_owner_service/entrypoint.sh
+++ e-2-e-tests/src/test/java/com/e2e_tests/main_owner_service/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 {
-  java -cp @/app/jib-classpath-file com.main_owner_service.run.MainOwnerServiceApplication
+  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -cp @/app/jib-classpath-file com.main_owner_service.run.MainOwnerServiceApplication
 } || {
   sleep infinity
 }

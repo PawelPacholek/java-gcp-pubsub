8d3c5e745b677f25ced3a6267954f54e219e2933
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-03-28 12:02:32 +0100

    wip

--- e-2-e-tests/src/test/java/com/e2e_tests/label_owner_service/entrypoint.sh
+++ e-2-e-tests/src/test/java/com/e2e_tests/label_owner_service/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 {
-  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006 -cp @/app/jib-classpath-file com.label_owner_service.run.LabelOwnerServiceApplication
+  #java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006 -cp @/app/jib-classpath-file com.label_owner_service.run.LabelOwnerServiceApplication
+  java -cp @/app/jib-classpath-file com.label_owner_service.run.LabelOwnerServiceApplication
+
 } || {
   sleep infinity
 }
--- main-owner-service/api/src/main/java/com/main_owner_service/api/controllers/UploadInitialOwnerController.java
+++ main-owner-service/api/src/main/java/com/main_owner_service/api/controllers/UploadInitialOwnerController.java
@@ -3,6 +3,8 @@ package com.main_owner_service.api.controllers;
 import com.main_owner_service.api.models.UploadedOwner;
 import com.main_owner_service.domain.usecases.UploadInitialOwnerUseCase;
 import com.main_owner_service.domain.models.InitialOwner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 public class UploadInitialOwnerController {
 
+    private static final Logger logger = LoggerFactory.getLogger("event logger");
+
     private final UploadInitialOwnerUseCase uploadOwnerUseCase;
 
     public UploadInitialOwnerController(UploadInitialOwnerUseCase uploadOwnerUseCase) {
@@ -24,6 +28,7 @@ public class UploadInitialOwnerController {
     public ResponseEntity<String> uploadOwner(@RequestBody UploadedOwner owner) {
         InitialOwner initialOwner = owner.toDomain();
         uploadOwnerUseCase.uploadInitialOwner(initialOwner);
+        logger.debug("---- owner upload completed ----");
         return new ResponseEntity<>("", HttpStatus.OK);
     }
 }

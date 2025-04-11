89a294c182a6c53c26ee9b9765831fc1867f70f3
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-04-11 11:26:43 +0200

    Debug both main-owner-service and label-owner-service

--- e-2-e-tests/src/test/java/com/e2e_tests/label_owner_service/entrypoint.sh
+++ e-2-e-tests/src/test/java/com/e2e_tests/label_owner_service/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 {
-  #java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006 -cp @/app/jib-classpath-file com.label_owner_service.run.LabelOwnerServiceApplication
-  java -cp @/app/jib-classpath-file com.label_owner_service.run.LabelOwnerServiceApplication
+  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006 -cp @/app/jib-classpath-file com.label_owner_service.run.LabelOwnerServiceApplication
 
 } || {
   sleep infinity

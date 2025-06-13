9495334a8e3691c107ece31e9866cde58906b7cc
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-06-13 09:41:43 +0200

    Switch Redis image to redis:8.0.2

--- main-owner-service/persistence/src/main/java/com/main_owner_service/persistence/PersistenceConfiguration.java
+++ main-owner-service/persistence/src/main/java/com/main_owner_service/persistence/PersistenceConfiguration.java
@@ -2,35 +2,18 @@ package com.main_owner_service.persistence;
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.google.cloud.spring.pubsub.integration.outbound.PubSubMessageHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.integration.annotation.Gateway;
-import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.handler.annotation.Header;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.KeyStore;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.time.Duration;
 
 @Configuration
 @EnableIntegration
@@ -38,79 +21,14 @@ public class PersistenceConfiguration {
 
   @Bean
   JedisConnectionFactory jedisConnectionFactory (
-    @Value("${session.state.datasource.host}") String host,
-    @Value("${session.state.datasource.port}") int port,
-    @Value("${session.state.datasource.password}") String password,
-    @Value("${session.state.datasource.certificate}") String certificateAsPem
-  ) throws Exception {
-    // 1. Załaduj certyfikat z formatu PEM
-    CertificateFactory cf = CertificateFactory.getInstance("X.509");
-    ByteArrayInputStream certInputStream = new ByteArrayInputStream(certificateAsPem.getBytes(StandardCharsets.UTF_8));
-    X509Certificate cert = (X509Certificate) cf.generateCertificate(certInputStream);
-
-    // 2. Utwórz TrustStore i załaduj certyfikat
-    KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-    trustStore.load(null); // pusty keystore
-    trustStore.setCertificateEntry("redis", cert);
-
-    // 3. Zbuduj TrustManager z TrustStore
-    TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-    tmf.init(trustStore);
-
-    // 4. Zbuduj SSLContext
-    SSLContext sslContext = SSLContext.getInstance("TLS");
-    sslContext.init(null, tmf.getTrustManagers(), null);
-
-    // 5. Skonfiguruj połączenie Redis z SSL
-    RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
-    redisConfig.setPassword(password);
-
-    JedisClientConfiguration clientConfig = JedisClientConfiguration.builder()
-      .useSsl()
-      .sslSocketFactory(sslContext.getSocketFactory())
-      //.connectTimeout(Duration.ofSeconds(5))
-      //.readTimeout(Duration.ofSeconds(5))
-      //.usePooling()
-      .build();
-
-    return new JedisConnectionFactory(redisConfig, clientConfig);
-
-    /*
-    JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-    jedisConFactory.setHostName(host);
-    jedisConFactory.setPort(port);
-    jedisConFactory.setPassword(password);
-    return jedisConFactory;
-    */
-
-  }
-
-/*
-  @Bean
-  public LettuceConnectionFactory redisConnectionFactory(
-    @Value("${session.state.datasource.host}") String host,
-    @Value("${session.state.datasource.port}") int port,
-    @Value("${session.state.datasource.password}") String password,
-    @Value("${session.state.datasource.certificate}") String certificateAsPem
+    @Value("${labeled.owner.host}") String host,
+    @Value("${labeled.owner.port}") int port
   ) {
-    RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-    configuration.setHostName(host);
-    configuration.setPort(port);
-    configuration.setPassword(password);
-    TrustManagerFactory instance = TrustManagers.factoryFrom(certificateAsPem);
-    ClientOptions sslOptions = ClientOptions.builder()
-      .sslOptions(SslOptions.builder().trustManager(instance).build())
-      .build();
-    return new LettuceConnectionFactory(
-      configuration,
-      LettuceClientConfiguration.builder()
-        .clientOptions(sslOptions)
-        .useSsl()
-        .disablePeerVerification()
-        .build()
-    );
+    RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+    JedisClientConfiguration clientConfig = JedisClientConfiguration.builder().build();
+    return new JedisConnectionFactory(redisConfig, clientConfig);
   }
-*/
+
   @Bean
   public RedisTemplate<Long, String> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
     RedisTemplate<Long, String> template = new RedisTemplate<>();
--- redis-instance/src/main/java/com/redis_instance/RedisInstance.java
+++ redis-instance/src/main/java/com/redis_instance/RedisInstance.java
@@ -4,14 +4,7 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.GenericContainer;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Map;
-import java.util.concurrent.Callable;
-
-import static java.nio.file.attribute.PosixFilePermissions.asFileAttribute;
-import static java.nio.file.attribute.PosixFilePermissions.fromString;
-import static java.util.Objects.requireNonNull;
 
 public class RedisInstance {
 
@@ -27,80 +20,26 @@ public class RedisInstance {
   private static Map<String, String> shared;
 
   private synchronized static void start() {
-    if (isAlreadyRunning())
-      return;
-    shared = instantiateRedis();
+    if (isNotRunning())
+      shared = instantiateRedis();
   }
 
-  private static boolean isAlreadyRunning() {
-    return shared != null;
+  private static boolean isNotRunning() {
+    return shared == null;
   }
 
   @SuppressWarnings("resource")
   private static Map<String, String> instantiateRedis() {
-    String tempDirectory = createTempDir();
-    copyFromResourcesTo("/redis/redis.crt", tempDirectory);
-    copyFromResourcesTo("/redis/redis.key.log", tempDirectory);
-    copyFromResourcesTo("/redis/redisCA.crt", tempDirectory);
-    GenericContainer<?> redis = new GenericContainer<>("bitnami/redis:7.4.2")
-      .withFileSystemBind(tempDirectory, "/certs")
-      .withEnv("REDIS_PASSWORD", "password")
-      .withEnv("REDIS_TLS_ENABLED", "yes")
-      .withEnv("REDIS_TLS_AUTH_CLIENTS", "no")
-      .withEnv("REDIS_TLS_PORT", "6379")
-      .withEnv("REDIS_TLS_CERT_FILE", "/certs/redis.crt")
-      .withEnv("REDIS_TLS_KEY_FILE", "/certs/redis.key")
-      .withEnv("REDIS_TLS_CA_FILE", "/certs/redisCA.crt")
-      .withExposedPorts(6379);
+    GenericContainer<?> redis = new GenericContainer<>("redis:8.0.2").withExposedPorts(6379);
     redis.start();
     return Map.of(
-      "session.state.datasource.host", redis.getHost(),
-      "session.state.datasource.port", String.valueOf(redis.getMappedPort(6379)),
-      "session.state.datasource.password", "password",
-      "session.state.datasource.certificate", InputStreams.asString(RedisInstance.class.getResourceAsStream("/redis/redisCA.crt"))
+      "labeled.owner.host", redis.getHost(),
+      "labeled.owner.port", String.valueOf(redis.getMappedPort(6379))
     );
-  }
-
-  private static String createTempDir() {
-    return unchecked(() -> Files.createTempDirectory("kx-redis", asFileAttribute(fromString("rwxrwxrwx")))).toString();
-  }
-
-  private static void copyFromResourcesTo(String resourcePath, String tempDirectory) {
-    unchecked(() -> {
-      Path filePath = Path.of(tempDirectory + textAfter(resourcePath, "/redis"));
-      Files.createFile(
-        filePath,
-        asFileAttribute(fromString("rwxrwxrwx"))
-      );
-      Files.write(
-        filePath,
-        InputStreams.asByteArray(requireNonNull(RedisInstance.class.getResourceAsStream(resourcePath)))
-      );
-      return null;
-    });
   }
 
   public static Map<String, String> getProperties() {
     return shared == null ? Map.of() : shared;
-  }
-
-  private static <T> T unchecked(Callable<T> callable) {
-    try {
-      return callable.call();
-    } catch (RuntimeException e) {
-      throw e;
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      throw new RuntimeException(e);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private static String textAfter(String text, String front) {
-    int beginIndex = text.indexOf(front) + front.length();
-    int endIndex = text.length();
-    return text.contains(front) ? text.substring(beginIndex, endIndex) : null;
   }
 
 }

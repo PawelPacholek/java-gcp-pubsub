24c7e9aae9e41715d7f9758e11c90abb4af82568
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-09-12 10:05:18 +0200

    Add fat jar for main-owner-service:run

--- main-owner-service/api/src/main/java/module-info.java
+++ main-owner-service/api/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module java.gcp.pubsub.main.owner.service.api.main {
     requires java.gcp.pubsub.main.owner.service.domain.main;
     requires com.example.fatmodule;
+    //exports org.springframework.boot to java.gcp.pubsub.main.owner.service.run.main;
 }
---
+++ main-owner-service/run/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module java.gcp.pubsub.main.owner.service.run.main {
+    requires java.gcp.pubsub.main.owner.service.api.main; //requires transitive ?
+    requires java.gcp.pubsub.main.owner.service.persistence.main;
+    requires com.example.fatrun;
+}

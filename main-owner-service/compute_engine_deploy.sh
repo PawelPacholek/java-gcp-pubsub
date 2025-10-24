b89aafac767e04b5ee00942fd8745a57ced7814f
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-10-24 12:38:28 +0200

    Add deployment script to compute engine for main-owner-service

---
+++ main-owner-service/compute_engine_deploy.sh
@@ -0,0 +1,17 @@
+# Parametry
+VM_NAME="main-owner-service-20251024-102351"
+ZONE="europe-central2-c"
+IMAGE="europe-central2-docker.pkg.dev/local-axle-425708-t0/project-repository/main-owner-service:latest"
+CONTAINER_NAME="main-owner-service-container"
+USER_NAME="pawel_pacholek"
+
+# Zdalny deploy przez SSH do maszyny
+gcloud compute ssh "$USER_NAME@$VM_NAME" --zone="$ZONE" --command="\
+  echo '===> Stopping old container (if any)...' && \
+  docker stop $CONTAINER_NAME || true && \
+  docker rm $CONTAINER_NAME || true && \
+  echo '===> Fetching new image...' && \
+  docker pull $IMAGE && \
+  echo '===> Starting new container...' && \
+  docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE \
+"

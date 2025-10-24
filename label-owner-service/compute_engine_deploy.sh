0d2fab30381f3d48605cf4a876e6cec72cb522e8
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-10-24 11:38:03 +0200

    Fix Compute engine deployment script - add user, add image pulling and fix port

--- label-owner-service/compute_engine_deploy.sh
+++ label-owner-service/compute_engine_deploy.sh
@@ -3,12 +3,15 @@ VM_NAME="label-owner-service-20251010-081236"
 ZONE="europe-central2-c"
 IMAGE="europe-central2-docker.pkg.dev/local-axle-425708-t0/project-repository/label-owner-service:latest"
 CONTAINER_NAME="label-owner-service-container"
+USER_NAME="pawel_pacholek"
 
 # Zdalny deploy przez SSH do maszyny
-gcloud compute ssh "$VM_NAME" --zone="$ZONE" --command="\
+gcloud compute ssh "$USER_NAME@$VM_NAME" --zone="$ZONE" --command="\
   echo '===> Stopping old container (if any)...' && \
   docker stop $CONTAINER_NAME || true && \
   docker rm $CONTAINER_NAME || true && \
+  echo '===> Fetching new image...' && \
+  docker pull $IMAGE && \
   echo '===> Starting new container...' && \
-  docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE \
+  docker run -d --name $CONTAINER_NAME -p 8081:8081 $IMAGE \
 "

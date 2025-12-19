aa2ca43fc58d0c687a6a59e2fadaac5052cf4112
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-12-19 10:33:52 +0100

    Update junit to version 6.0.1

--- e-2-e-tests/build.gradle.kts
+++ e-2-e-tests/build.gradle.kts
@@ -17,13 +17,13 @@ dependencies {
 
     testImplementation(project(":pubsub-emulator"))
     testImplementation(project(":redis-instance"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.1")
     testImplementation("org.springframework.boot:spring-boot-test-autoconfigure:3.3.3")
     testImplementation("org.testcontainers:testcontainers:2.0.2")
     testImplementation("com.google.cloud.tools:jib-core:0.28.1")
     testImplementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.1")
 
 }
 

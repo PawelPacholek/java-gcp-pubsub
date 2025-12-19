f49e8e261e15f8b34f49ea42baa1ea83da7ffcfc
Paweł Pachołek <pawel.pacholek@kontomatik.com>
2025-12-19 10:17:12 +0100

    Update gradle to version 9.2.1

--- main-owner-service/api/src/test/java/com/main_owner_service/api/MainOwnerServiceApplication.java
+++ main-owner-service/api/src/test/java/com/main_owner_service/api/MainOwnerServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainOwnerServiceApi {
+public class MainOwnerServiceApplication {
   public static void main(String[] args) {
-    SpringApplication.run(MainOwnerServiceApi.class, args);
+    SpringApplication.run(MainOwnerServiceApplication.class, args);
   }
 }
